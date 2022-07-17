9bc72ea39305ca298aa233f87fe013ae2b30573c
umiic <18ucheema@mtsn.org.uk>
2022-07-17 10:58:25 +0100

    all files inc. KNN + LR

---
+++ KNN/KNN Working File.py
@@ -0,0 +1,8 @@
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+data = pd.read_csv("car.data")
